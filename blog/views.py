b8214edeec50aeefa4be340e66b32c184688faaf
PablitoGarcia <garciasantione@gmail.com>
2015-09-28 08:12:01 -0300

    Readme modificado y anteproyecto

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,9 @@
 from django.template import RequestContext
 from django.shortcuts import render_to_response, render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def home(request):
     context = RequestContext(request)
@@ -13,3 +17,35 @@ def login(request):
 def registro(request):
     return render_to_response('register.html',
                               {})
+
+def nuevo_usuario(request):
+    if request.method=='POST':
+        formulario = UserCreationForm(request.POST)
+        if formulario.is_valid:
+            formulario.save()
+            return HttpResponseRedirect('/')
+    else:
+        formulario = UserCreationForm()
+    return render_to_response('nuevousuario.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
+def ingresar(request):
+    if not request.user.is_anonymous():
+        return HttpResponseRedirect('/privado')
+    if request.method == 'POST':
+        formulario = AuthenticationForm(request.POST)
+        if formulario.is_valid:
+            usuario = request.POST['username']
+            clave = request.POST['password']
+            acceso = authenticate(username=usuario, password=clave)
+            if acceso is not None:
+                if acceso.is_active:
+                    login(request, acceso)
+                    return HttpResponseRedirect('/privado')
+                else:
+                    return render_to_response('noactivo.html', context_instance=RequestContext(request))
+            else:
+                return render_to_response('nousuario.html', context_instance=RequestContext(request))
+    else:
+        formulario = AuthenticationForm()
+    return render_to_response('ingresar.html',{'formulario':formulario}, context_instance=RequestContext(request))
+
