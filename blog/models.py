2ddacd7a0eaa6302091e0feadf1a51951c9935d1
panchovil <matiasrafaelferreyra@gmail.com>
2015-10-09 11:16:15 -0300

    base de datos final y cambios en views y form

--- blog/models.py
+++ blog/models.py
@@ -1,25 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.db import models
 from django.contrib.auth.models import User
-global puntaje      
 
+class Categoria(models.Model):
+    nombre = models.CharField("Nombre de la Categoria", max_length=50)
+    descripcion = models.TextField(u'Descripcion de la categoria')
 
-class Entrada(models.Model):
-    class Meta:
-        verbose_name = "Mi Entrada"
-        verbose_name_plural = "Todas mis entradas"
-        ordering=['-fecha']
-    
-    titulo = models.CharField(u'Título', max_length=100)
-    fecha = models.DateTimeField(u'Fecha del Post',auto_now_add=True)
-    resumen= models.CharField(u'Resumen',max_length=512)
-    contenido = models.TextField(u'Contenido')
-    published = models.BooleanField(u'Publicado?', default=True)
-    autor = models.ForeignKey(User)
-    categoria = models.ManyToManyField('Categoria')
-    
     def __str__(self):
-        return self.titulo
+        return self.nombre
+
     
 class Juego(models.Model):
     class Meta:
@@ -30,33 +19,23 @@ class Juego(models.Model):
     imagen = models.FileField(u'Caratula de Juego', upload_to='imagendeljuego', default='null')
     titulo = models.CharField(u'Título', max_length=100)
     fecha = models.DateTimeField(u'Fecha del Post',auto_now_add=True)
-    resumen= models.CharField(u'Resumen',max_length=512)
-    critica = models.TextField(u'Critica')
+    contenido = models.TextField(u'Contenido')
     publicado = models.BooleanField(u'Publicado', default=True)
     autor = models.ForeignKey(User)
-    tag = models.ManyToManyField('Categoria')
-    puntajeSegunAutor = models.IntegerField(u'Puntaje por el autor')
-    
+    categoria = models.ManyToManyField(Categoria)
+    url = models.CharField(u'Link del Trailer', max_length=200)
+
     def __str__(self):
         return self.titulo
-
-    
-class Categoria(models.Model):
-    nombre = models.CharField("Nombre de la Categoria", max_length=50)
-    descripcion = models.TextField(u'Descripcion de la categoria')
-
-    def __str__(self):
-        return self.nombre
-    
 
 class Comentario(models.Model):
     class Meta:
         ordering=['fecha']
-    post = models.ForeignKey(Entrada)
+    post = models.ForeignKey(Juego)
     nick = models.CharField('publicado por:', max_length=50)
     contenido = models.TextField('Comentario', max_length=500)
     fecha = models.DateTimeField('Fecha', auto_now_add=True)
-    
+    puntajeSegunComunidad = models.IntegerField(u'puntajeSegunComunidad')    
         
     def __str__(self):
-        return self.nick, self.puntaje
+        return self.nick, self.puntajeSegunComunidad
