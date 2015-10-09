52c9beb1e0112d2b7b21eb828aed90335f547f7e
PablitoGarcia <garciasantione@gmail.com>
2015-10-09 08:38:32 -0300

    Update forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -13,24 +13,14 @@ class SignUpForm(ModelForm):
         model = User
         fields = ['username', 'password1' , 'password2' , 'email' , 'first_name', 'last_name']
         widgets = {
-<<<<<<< HEAD
+
             'password1': forms.PasswordInput(),
             'password2': forms.PasswordInput(),
         }
         
-    def save(self, commit=True):
-        user = super(UserCreateForm, self).save(commit=False)
-        user.email = self.cleaned_data["email"]
-        if commit:
-            user.save()
-        return user
-
-
-=======
-            'password': forms.PasswordInput(),
-        }
+    
 class PostForm(forms.ModelForm):
     class Meta:
         model = Entrada
         fields = ('titulo', 'resumen', 'contenido', 'published', 'categoria')
->>>>>>> 10a1942eb906a899c3f2c813b9faf0d865cd3e4c
+
